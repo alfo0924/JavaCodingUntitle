7ac4d9519839c3184862561d5ee97e1868b06685
user <d1204433@o365.fcu.edu.tw>
2024-05-09 15:36:43 +0800

    commit n push 1.5

---
+++ src/main/java/org/example/c64Extend.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import java.util.*;
+
+class cbba
+{
+
+    String status;
+
+}
+
+
+//藉extends 繼承父層
+class abpa extends  cbba
+{
+int id;
+
+
+
+
+}
+//藉extends 繼承父層
+class baba extends abpa
+{
+
+ void printOut()
+ {
+     System.out.println("id : "+id);
+     System.out.println("status : "+status);
+
+ }
+
+
+}
+
+public class c64Extend
+{
+    public static void main(String[]args)
+    {
+        cbba ca=new cbba();
+        baba b=new baba();
+
+        b.printOut();
+        System.out.println(" \nblank test");
+
+
+        //呼叫子層的方法
+        b.printOut();
+        System.out.println(" \nblank test");
+
+        //帶入值並呼叫方法
+        b.id=2;
+        b.status="gud";
+        b.printOut();
+//        System.out.println();
+
+
+
+
+    }
+
+
+
+}
