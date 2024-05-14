25548567d9038f0a837573ebae040c731f2b6951
user <d1204433@o365.fcu.edu.tw>
2024-05-14 10:13:11 +0800

    commit n push 1.5

--- src/main/java/org/example/c64Class.java
+++ src/main/java/org/example/c64Class.java
@@ -7,6 +7,11 @@ class c64p
     {
         System.out.println("Hello from java");
     }
+    void act1()
+    {
+        System.out.println("Act1");
+    }
+
 }
 class c64c extends c64p
 {
@@ -24,9 +29,9 @@ class c64c extends c64p
         System.out.println("Hello from java3");
     }
 
-    void act()
+    void act2()
     {
-        System.out.println("Act1");
+        System.out.println("Act2");
     }
 
 
@@ -42,44 +47,51 @@ public class c64Class
     c64p cp = new c64p(); //父
     c64c cc = new c64c(); //子
 
-
+    System.out.println(" Default value : ");
 
     cp.printOut();
+    cp.act1();
     cc.printOut2();
     cc.printOut();  //印出override
-    cc.act();
+    cc.act1();
+    cc.act2();
 
-    System.out.println("\nTEst1"); //測試 區分用
+    System.out.println("\nTEst1 向上轉型 "); //測試 區分用
 
         c64p cp2=new c64c(); //向上轉型
 
     cp2.printOut(); //呼叫方法
-    cc.act(); // 印出act值
+    cp2.act1(); // 印出act值
 
 
-        System.out.println("\nTEst2");
+//        cp2.act2(); //無法印出子類別
+
+
+        System.out.println("\nTEst2 向下轉型");
 
 //    強制轉child型別  向下轉型(手動)
     c64c cp1 =(c64c)cp2;
 
         cp1.printOut();
         cp1.printOut2();
-        cp1.act();
+        cp1.act1();
+        cp1.act2();
 
 
 //        c64c cp12 =(c64c)cp;
 //
 //        cp12.printOut();
 //        cp12.printOut2();
-//        cp12.act();
+//        cp12.act1();
+//        cp12.act2();
 
-
-        System.out.println("\nTEst3");
+        System.out.println("\nTEst3 向下轉型");
 
     c64c cp11=(c64c)cp2;
         cp11.printOut();
         cp11.printOut2();
-        cp11.act();
+        cp11.act1();
+        cp11.act2();
 
 
     }
