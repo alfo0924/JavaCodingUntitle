2c9d883edd9c1a685d00b7e3314ffbfc2f6e10ff
user <d1204433@o365.fcu.edu.tw>
2024-05-14 09:59:06 +0800

    commit n push 1.5

--- src/main/java/org/example/c64Class.java
+++ src/main/java/org/example/c64Class.java
@@ -17,24 +17,69 @@ class c64c extends c64p
 
     //override 改寫父類
     @Override
-    void printOut() {
+    void printOut()
+    {
 
 //        super.printOut();
         System.out.println("Hello from java3");
     }
+
+    void act()
+    {
+        System.out.println("Act1");
+    }
+
+
 }
+
+
 public class c64Class
 {
 
     public  static void main(String[]args)
     {
 
-    c64p cp = new c64p();
-    c64c cc = new c64c();
+    c64p cp = new c64p(); //父
+    c64c cc = new c64c(); //子
+
+
 
     cp.printOut();
     cc.printOut2();
     cc.printOut();  //印出override
+    cc.act();
+
+    System.out.println("\nTEst1"); //測試 區分用
+
+        c64p cp2=new c64c(); //向上轉型
+
+    cp2.printOut(); //呼叫方法
+    cc.act(); // 印出act值
+
+
+        System.out.println("\nTEst2");
+
+//    強制轉child型別  向下轉型(手動)
+    c64c cp1 =(c64c)cp2;
+
+        cp1.printOut();
+        cp1.printOut2();
+        cp1.act();
+
+
+//        c64c cp12 =(c64c)cp;
+//
+//        cp12.printOut();
+//        cp12.printOut2();
+//        cp12.act();
+
+
+        System.out.println("\nTEst3");
+
+    c64c cp11=(c64c)cp2;
+        cp11.printOut();
+        cp11.printOut2();
+        cp11.act();
 
 
     }
