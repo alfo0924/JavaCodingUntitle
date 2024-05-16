d5665f9cdeba6800d84fb1c0dd01bf2388b28d72
user <d1204433@o365.fcu.edu.tw>
2024-05-16 23:16:36 +0800

    commit n push 1.81

--- src/main/java/org/example/Q22.java
+++ src/main/java/org/example/Q22.java
@@ -1,46 +1,36 @@
 package org.example;
-import java.util.*;
+import java.util.*; //Util套件
 
 public class Q22 {
     public static void main(String[]args)
     {
 
-
-
-
-        boolean input=false;
-        while(!input)
+        boolean input=false; //建立Boolean 預設值:false
+        while(!input) //建立循環條件 若等於true就會中斷
         {
-
-            Scanner scanner= new Scanner(System.in);
+            Scanner scanner= new Scanner(System.in); //建立Scanner 套件
 
             System.out.println("Please input your last name and first name: ");
             System.out.println("last name :");
-            String lastname=scanner.nextLine();
+            String lastname=scanner.nextLine(); //建立 輸入String lastname 的scanner
             System.out.println("first name :");
-            String firstname=scanner.nextLine();
+            String firstname=scanner.nextLine();//建立 輸入String firstname的scanner
 
-            if(!lastname.equals(firstname) )
+            if(!lastname.equals(firstname) )//建立判斷式(若lastname字串比對相等於firstname)
             {
-
+                //若條件相符 就會 把lastname/firstname藉.toUpperCase()轉換成大寫
+                //要印出 " 需要用 \" 即可印出
                 System.out.println(" \"Hello "+lastname.toUpperCase() +","+
                         firstname.toUpperCase()+"!\"" );
-                input=true;
+                input=true; //條件滿足Boolean 值設為true 離開循環
             }
             else
             {
-                System.out.println("\nIncorrect! Please input your name again : \n");
-                input=false;
+                System.out.println("\nIncorrect! Please input your name again : \n"); //輸入錯誤時顯現告示使用者
+                input=false; //條件不滿足Boolean 值設為false 繼續循環
             }
-
-
-
         }
 
-
-
-
     }
-
 
 }
