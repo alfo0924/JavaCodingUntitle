b93a17b8a0bdf60874f8dc9fdfce10f99c6e9575
user <d1204433@o365.fcu.edu.tw>
2024-06-06 16:13:16 +0800

    commit n push 3.21

--- src/main/java/org/example/C65BMI.java
+++ src/main/java/org/example/C65BMI.java
@@ -1,12 +1,11 @@
 package org.example;
 import java.util.Scanner;
- class InvalidNumberException extends Exception {
-
-    public  InvalidNumberException ()
+ class IOException extends Exception {
+    public  IOException()
     {
 
     }
-    public InvalidNumberException(String message)
+    public IOException(String message)
     {
         super(message);
     }
@@ -26,20 +25,25 @@ class BmiUtil
     }
     void getBmi()
     {
-        if(height>=2.5&&height<=250) {               //若身高單位是公分
-            double Height = height / 100;           //把身高數值/100 轉換公尺
-            double sqrheight = Math.pow(Height, 2); //身高平方 藉數學套件 Math.pow(身高,2次方)
-            double BMI = weight / sqrheight;        //BMI 公式
-            double bmi = Math.round(BMI * 100) * 0.01; //把BMI的值 *100後四捨五入在*0.01 為了取小數點後兩位
-            System.out.println("BMI :" + bmi); //印出BMI值
+        try {
+            if (height >= 2.5 && height <= 250) {               //若身高單位是公分
+                double Height = height / 100;           //把身高數值/100 轉換公尺
+                double sqrheight = Math.pow(Height, 2); //身高平方 藉數學套件 Math.pow(身高,2次方)
+                double BMI = weight / sqrheight;        //BMI 公式
+                double bmi = Math.round(BMI * 100) * 0.01; //把BMI的值 *100後四捨五入在*0.01 為了取小數點後兩位
+                System.out.println("BMI :" + bmi); //印出BMI值
+            } else {
+                double sqrheight = Math.pow(height, 2); //身高平方 藉數學套件 Math.pow(身高,2次方)
+                double BMI = weight / sqrheight;        //BMI 公式
+                double bmi = Math.round(BMI * 100) * 0.01; //把BMI的值 *100後四捨五入在*0.01 為了取小數點後兩位
+                System.out.println("BMI :" + bmi);      //印出BMI值
+            }
         }
-        else
-        {
-            double sqrheight = Math.pow(height, 2); //身高平方 藉數學套件 Math.pow(身高,2次方)
-            double BMI = weight / sqrheight;        //BMI 公式
-            double bmi =Math.round(BMI*100)*0.01 ; //把BMI的值 *100後四捨五入在*0.01 為了取小數點後兩位
-            System.out.println("BMI :" + bmi);      //印出BMI值
+        catch (Exception e) {
+
+            System.out.println(e);
         }
+
     }
 }
 public class C65BMI {
@@ -49,7 +53,8 @@ public class C65BMI {
         try{                                //放入可能發生例外的部分
             bmi.setHeightsetWeight();
             bmi.getBmi();
-//            throw new InvalidNumberException();
+//            throw new IOException();
+
         }
         catch (Exception e)                  //若執行錯誤就會印出錯誤
         {
