ea8108f5e427000be9526f325a2eebbd0f82154f
user <d1204433@o365.fcu.edu.tw>
2024-06-06 14:50:15 +0800

    commit n push 3.1

--- src/main/java/org/example/C65BMI.java
+++ src/main/java/org/example/C65BMI.java
@@ -1,6 +1,5 @@
 package org.example;
 import java.util.Scanner;
-//import java.util.*;
 class BmiUtil
 {   private double height;
     private double weight;
@@ -16,34 +15,36 @@ class BmiUtil
     }
     void getBmi()
     {
-        if(height>=2.5&&height<=250) {
-            double Height = height / 100; //把身高數值/100 轉換公尺
+        if(height>=2.5&&height<=250) {               //若身高單位是公分
+            double Height = height / 100;           //把身高數值/100 轉換公尺
             double sqrheight = Math.pow(Height, 2); //身高平方 藉數學套件 Math.pow(身高,2次方)
-            double BMI = weight / sqrheight; //BMI 公式
+            double BMI = weight / sqrheight;        //BMI 公式
             double bmi = Math.round(BMI * 100) * 0.01; //把BMI的值 *100後四捨五入在*0.01 為了取小數點後兩位
             System.out.println("BMI :" + bmi); //印出BMI值
         }
         else
         {
             double sqrheight = Math.pow(height, 2); //身高平方 藉數學套件 Math.pow(身高,2次方)
-            double BMI = weight / sqrheight; //BMI 公式
+            double BMI = weight / sqrheight;        //BMI 公式
             double bmi =Math.round(BMI*100)*0.01 ; //把BMI的值 *100後四捨五入在*0.01 為了取小數點後兩位
-            System.out.println("BMI :" + bmi); //印出BMI值
+            System.out.println("BMI :" + bmi);      //印出BMI值
         }
     }
 }
-
 public class C65BMI {
     public static void main(String[] args) {
         System.out.println("Bmi System :");
         BmiUtil bmi = new BmiUtil();
-        try{
+        try{                                //放入可能發生例外的部分
             bmi.setHeightsetWeight();
             bmi.getBmi();
         }
-        catch (Exception e)
+        catch (Exception e)                  //若執行錯誤就會印出錯誤
         {
             System.out.println("Error input");
+        }
+        finally {
+            System.out.println("Bmi System execute "); // 無論如何都會被執行
         }
     }
 }
